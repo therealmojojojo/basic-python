88b5ae32cdde901179786f4affff96e1fe64505a
Costin <costin.genescu@gmail.com>
2022-02-10 18:29:50 +0200

    .

--- converter/ingfinance.py
+++ converter/ingfinance.py
@@ -1,27 +1,42 @@
+import pandas
+import glob
+from datetime import datetime
 
-
-source_file_name = "C:\\Users\\costi\\Downloads\\Tranzactii_pe_perioada - ianuarie.csv"
-target_file_name = "C:\\Users\\costi\\Downloads\\Tranzactii_pe_perioada - ianuarie - processed.csv"
+input_folder = "D:\\Users\\costin\\Downloads\\"
+processed_sufix = "- processed.csv"
 separator = "|"
 
-output_file = open(target_file_name, "a")
+current_date = datetime.today().strftime("%d-%m-%Y")
+print(current_date);
+
+files = glob.glob(input_folder + "Tranzactii_" + current_date + "*csv")
+files = [x for x in files if not x.find(processed_sufix) != -1]
+print (files)
+
 def write_line(line):
     output_file.write(line + "\n")
 
-input_file = open(source_file_name)
+for filename in files:
+    output_file = open(filename + processed_sufix, "w")
+    
+    input_file = open(filename)
 
-buffer = ""
-for line in input_file:
-    line = line[:-1]
-    if line[0].isnumeric():
-        write_line(buffer)
-        buffer = line
-    else:
-        buffer += line + separator
-write_line(buffer)
+    buffer = ""
+    for line in input_file:
+        line = line[:-1]
+        if line[0].isnumeric():
+            buffer = buffer.replace(':','')
+            write_line(buffer)
+            buffer = line
+        else:
+            buffer += line + separator
+    buffer = buffer.replace(':','')
+    write_line(buffer)
 
-output_file.close()
-input_file.close()
+    output_file.close()
+    input_file.close()
+
+print("Done")
 
 
 
